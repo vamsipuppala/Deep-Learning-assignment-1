508598784af1df4ee9dcd3337e877e29df5f0f1f
vamsipuppala <puppala@usc.edu>
2023-02-26 18:08:55 -0800

    2nd push

--- lib/cnn/cnn_models.py
+++ lib/cnn/cnn_models.py
@@ -11,6 +11,10 @@ from lib.cnn.layer_utils import *
 class TestCNN(Module):
     def __init__(self, dtype=np.float32, seed=None):
         self.net = sequential(
+            ConvLayer2D(input_channels=3, kernel_size=3, number_filters=3,  name="conv_test"),
+            MaxPoolingLayer(2,2,'pooling'),
+            flatten("flat"),
+            fc(27,5, 0.02, "fc")
             ########## TODO: ##########
             ########### END ###########
         )
@@ -19,6 +23,15 @@ class TestCNN(Module):
 class SmallConvolutionalNetwork(Module):
     def __init__(self, keep_prob=0, dtype=np.float32, seed=None):
         self.net = sequential(
+            ConvLayer2D(input_channels=3, kernel_size=3, number_filters=16,  padding=1,  name="conv_test1"),
+            gelu(name="re1"),
+            ConvLayer2D(input_channels=16, kernel_size=3, number_filters=32, stride=2, name="conv_test2"),
+            gelu(name="re12"),
+            MaxPoolingLayer(2,2,'pooling'),
+            flatten("flat"),
+            fc(1568,100, 5e-2, "fc1"),
+            gelu(name="re13"),
+            fc(100,20, 5e-2, "fc2")
             ########## TODO: ##########
             ########### END ###########
         )
--- lib/cnn/layer_utils.py
+++ lib/cnn/layer_utils.py
@@ -129,25 +129,107 @@ class ConvLayer2D(object):
         # TODO: Implement the calculation to find the output size given the         #
         # parameters of this convolutional layer.                                   #
         #############################################################################
-        pass
+        
+        output_shape[0] = input_size[0]
+        output_shape[1] = ((input_size[1]+2*self.padding-self.kernel_size)//(self.stride))+1
+        output_shape[2] = ((input_size[2]+2*self.padding-self.kernel_size)//(self.stride))+1
+        output_shape[3] =  self.number_filters
         #############################################################################
         #                             END OF YOUR CODE                              #
         #############################################################################
         return output_shape
+#     def conv_mult(self, img_val, filter_weights, b):
+# #         ans = np.sum(img_val * filter_weights, axis=(1,2,3))
+#         temp = np.multiply(img_val,filter_weights)
+#         ans = np.sum(temp)
+# #         ans = ans + b
+#         return ans
+    def do_pad(self, img, pad):
+        return np.pad(img, ((0,0),(pad,pad),(pad,pad),(0,0)))
 
     def forward(self, img):
         output = None
         assert len(img.shape) == 4, "expected batch of images, but received shape {}".format(img.shape)
-
+#         print(img.shape)
         output_shape = self.get_output_size(img.shape)
-        _ , input_height, input_width, _ = img.shape
+        _, input_height, input_width, _ = img.shape
         _, output_height, output_width, _ = output_shape
+#         print(self.params['conv_test_w'].shape)
 
         #############################################################################
         # TODO: Implement the forward pass of a single convolutional layer.       #
         # Store the results in the variable "output" provided above.                #
         #############################################################################
-        pass
+        bt = img.shape[0]
+        input_img = self.do_pad(img, self.padding)
+#         img_pad = np.pad(img,((0,0),(self.padding,self.padding),(self.padding,self.padding),(0,0)))
+        val = np.newaxis
+        output = np.zeros((bt,output_height, output_width,  self.number_filters ))
+        for i in range(output_height):
+            i1 = self.stride * i 
+            j1 = i1  + self.kernel_size
+            for j in range(output_width):
+                i2 = self.stride * j
+                j2 = i2 + self.kernel_size
+                mult_mesh = input_img[:,i1:j1, i2:j2,:, val]
+                filter_weights = self.params[self.w_name][val:,:,:]
+                ans = np.sum(mult_mesh * filter_weights, axis=(1,2,3))
+                output[:,i,j,:] = ans
+            
+        output += self.params[self.b_name]  
+        
+        
+#         for b in range(0, bt):
+#             bt_li = []
+#             for i in range(output_height):
+#                 i1 = self.stride * i 
+#                 j1 = i1  + self.kernel_size
+#                 for j in range(output_width):
+#                     i2 = self.stride * j
+#                     j2 = i2 + self.kernel_size
+#                     for k in range(self.number_filters):
+#                         filter_weights = self.params[self.w_name][:,:,:,k]
+#                         bias_values = self.params[self.b_name][k]
+#                         one_box = img_pad[b, i1:j1, i2:j2,:]
+                        
+#                         output[b, i, j, k] = self.conv_mult(one_box, filter_weights, bias_values)
+        
+            
+#         for i in range(output_height):
+#             i1 = self.stride * i 
+#             j1 = i1  + self.kernel_size
+#             for j in range(output_width):
+#                 i2 = self.stride * j
+#                 j2 = i2 + self.kernel_size
+#                 for k in range(self.number_filters):
+#                     filter_weights = self.params[self.w_name][:,:,:,k]
+#                     bias_values = self.params[self.b_name][k]
+#                     one_box = img_pad[:, i1:j1, i2:j2,:]
+
+#                     output[:, i, j, k] = self.conv_mult(one_box, filter_weights, bias_values)
+#         output += self.params[self.b_name] 
+#         for i in range(output_height):
+#             i1 = self.stride * i 
+#             j1 = i1  + self.kernel_size
+#             for j in range(output_width):
+#                 i2 = self.stride * j
+#                 j2 = i2 + self.kernel_size
+                
+#                 filter_weights = self.params[self.w_name][:,:,:,:]
+#                 print("***")
+#                 print(filter_weights.shape)
+#                 bias_values = self.params[self.b_name]
+#                 one_box = img[:, i1:j1, i2:j2,:]
+#                 print(one_box.shape)
+#                 output[:, i, j, :] = self.conv_mult(one_box, filter_weights, bias_values) 
+#                 output += self.params[self.b_name] 
+
+                        
+                        
+                        
+                      
+
+            
         #############################################################################
         #                             END OF YOUR CODE                              #
         #############################################################################
@@ -155,21 +237,44 @@ class ConvLayer2D(object):
         
         return output
 
-
+    def add_pad(self, dimg, img, p):
+        te = (0,0)
+        dimg_pad = np.pad(dimg, (te,(p,p),(p,p),te))
+        img_pad = np.pad(img, (te,(p,p),(p,p),(0,0)))
+        return dimg_pad, img_pad
     def backward(self, dprev):
         img = self.meta
         if img is None:
             raise ValueError("No forward function called before for this module!")
 
         dimg, self.grads[self.w_name], self.grads[self.b_name] = None, None, None
-        
+        self.grads[self.b_name] = np.zeros(self.number_filters)
+        r1_r2_r3 = (0,1,2)
+        self.grads[self.b_name] = np.sum(dprev,axis=r1_r2_r3) 
+        val = np.newaxis
         #############################################################################
         # TODO: Implement the backward pass of a single convolutional layer.        #
         # Store the computed gradients wrt weights and biases in self.grads with    #
         # corresponding name.                                                       #
         # Store the output gradients in the variable dimg provided above.           #
         #############################################################################
-        pass
+        dimg = np.zeros_like(img)
+        _, h, w, _ = dprev.shape
+        self.grads[self.w_name] = np.zeros((self.kernel_size, self.kernel_size, self.input_channels,self.number_filters))
+        
+        dimg_p, img_p = self.add_pad(dimg, img, self.padding)
+        
+        for i in range(h):
+            i1 = self.stride * i 
+            j1 = i1  + self.kernel_size
+            for j in range(w):
+                i2 = self.stride * j
+                j2 = i2 + self.kernel_size
+                self.grads[self.w_name] += np.sum(img_p[:, i1:j1, i2:j2, :, val] *dprev[:, i:i+1, j:j+1, val, :],axis=0)
+                dimg_p[:,i1:j1,i2:j2,:] += np.sum(self.params[self.w_name][np.newaxis,:,:,:,:] * dprev[:,i:i+1,j:j+1,np.newaxis,:], axis= 4)
+
+        dimg = dimg_p[:,self.padding:img.shape[1] + self.padding, self.padding:img.shape[1] + self.padding,:]
+            
         #############################################################################
         #                             END OF YOUR CODE                              #
         #############################################################################
@@ -195,27 +300,67 @@ class MaxPoolingLayer(object):
         # TODO: Implement the forward pass of a single maxpooling layer.            #
         # Store your results in the variable "output" provided above.               #
         #############################################################################
-        pass
+        
+        _, input_height, input_width, _ = img.shape
+        n_H = int(1 + (input_height - self.pool_size) // self.stride)
+        n_W = int(1 + (input_width - self.pool_size) // self.stride)
+        
+
+        # Initialize output matrix A
+        bt = img.shape[0]
+        output = np.zeros((bt, n_H,  n_W,  img.shape[3] ))
+        for i in range(n_H):
+            i1 = self.stride * i 
+            j1 = i1  + self.pool_size
+            for j in range(n_W):
+                i2 = self.stride * j
+                j2 = i2 + self.pool_size
+                
+                output[:, i, j, :] = np.max(img[:,i1:j1,i2:j2,:], axis=(1,2))
+        
+#         for i in range(n_H):
+#             i1 = self.stride * i 
+#             j1 = i1  + self.pool_size
+#             for j in range(n_W):
+#                 i2 = self.stride * j
+#                 j2 = i2 + self.pool_size
+#                 for k in range(img.shape[3]):
+#                     output[:, i, j, k] = np.max(img[:,i1:j1,i2:j2,k])
+        
+                        
         #############################################################################
         #                             END OF YOUR CODE                              #
         #############################################################################
         self.meta = img
 
         return output
+    
 
     def backward(self, dprev):
         img = self.meta
 
         dimg = np.zeros_like(img)
-        _, h_out, w_out, _ = dprev.shape
+        _, h_out, w_out,_ = dprev.shape
         h_pool, w_pool = self.pool_size,self.pool_size
-
+        
         #############################################################################
         # TODO: Implement the backward pass of a single maxpool layer.              #
         # Store the computed gradients in self.grads with corresponding name.       #
         # Store the output gradients in the variable dimg provided above.           #
         #############################################################################
-        pass
+        for v in range(dprev.shape[0]):
+            img_init_values = img[v]
+            for i in range(h_out):
+                i1 = self.stride * i 
+                j1 = i1  + h_pool
+                for j in range(w_out):
+                    i2 = self.stride * j
+                    j2 = i2 + w_pool
+                    for k in range(dprev.shape[3]):
+                        box = img_init_values[i1:j1,i2:j2,k]
+                        dimg[v, i1:j1,i2:j2,k] += np.multiply((box == np.max(box)),dprev[v, i, j,k])
+       
+        
         #############################################################################
         #                             END OF YOUR CODE                              #
         #############################################################################
